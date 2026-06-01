71b1a70557aab875684609f07dc46736da8a559a
Samia <sinan2003bin@gmail.com>
2026-06-01 12:16:39 +0600

    Created a new project and tested workability

--- gmail_account/views.py
+++ gmail_account/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from urllib import request
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+#from django.contrib.auth.models import User
+#from django.contrib.auth import authenticate, login
+#from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, "home.html")
